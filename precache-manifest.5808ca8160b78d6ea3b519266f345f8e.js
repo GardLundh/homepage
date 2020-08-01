973ee317d2d7defc43ce35055bdfcf3c01b46716
GardLundh <gard.lundh@gmail.com>
2020-08-01 11:10:25 +0200

    Updates

---
+++ precache-manifest.5808ca8160b78d6ea3b519266f345f8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4ed3b9894637bc4b8274b13c3e734c",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "cfa67ab778409440982a",
+    "url": "/homepage/static/css/main.c8cfd277.chunk.css"
+  },
+  {
+    "revision": "3b4e651ddf745785ac8f",
+    "url": "/homepage/static/js/2.de4630bf.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/homepage/static/js/2.de4630bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa67ab778409440982a",
+    "url": "/homepage/static/js/main.fc5e415d.chunk.js"
+  },
+  {
+    "revision": "20eb30a220522f5bd503",
+    "url": "/homepage/static/js/runtime-main.a8951460.js"
+  },
+  {
+    "revision": "a30ed51f5b37a31bbff72992f72ca183",
+    "url": "/homepage/static/media/covidTracker.a30ed51f.jpg"
+  },
+  {
+    "revision": "648ede64a5f0c75dc2b798cf612346a8",
+    "url": "/homepage/static/media/profilepicture.648ede64.jpg"
+  },
+  {
+    "revision": "45dd5702a01dc2c12fabf979a44cdc40",
+    "url": "/homepage/static/media/shakespeareGenerator.45dd5702.jpg"
+  }
+]);
